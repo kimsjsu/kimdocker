ce70661a52ed36c3804f760d864ae3d0ab2bcec5
Suneuy Kim <suneuy.kim@sjsu.edu>
2020-01-20 21:41:07 -0800

    v0.6

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,20 +18,16 @@ cat > Dockerfile <<EOF
 FROM mongo:latest
 RUN apt-get update
 RUN apt-get upgrade -y
-RUN apt-get install -y vim wamerican iputils-ping iproute2 telnet python3 
-RUN apt-get install -y python3-pip
-RUN pip3 install pymongo
+RUN apt-get install -y vim wamerican iputils-ping iproute2 telnet
 EOF
 
-docker build . -t "myimage:Dockerfile"
+docker build -t "myimage:Dockerfile" .
 
 docker network create --subnet 192.168.10.0/24 --ip-range 192.168.10.0/24 my-mongo-cluster
 
-docker run -d --name c0 --ip 192.168.10.100 --network my-mongo-cluster myimage:Dockerfile mongod --replSet testset
-docker run -d --name c1 --ip 192.168.10.101 --network my-mongo-cluster myimage:Dockerfile mongod --replSet testset
-docker run -d --name c2 --ip 192.168.10.102 --network my-mongo-cluster myimage:Dockerfile mongod --replSet testset
-
-sleep 3
+docker run -d --name c0 --network my-mongo-cluster --ip 192.168.10.100 myimage:Dockerfile mongod --replSet testset
+docker run -d --name c1 --network my-mongo-cluster --ip 192.168.10.101 myimage:Dockerfile mongod --replSet testset
+docker run -d --name c2 --network my-mongo-cluster --ip 192.168.10.102 myimage:Dockerfile mongod --replSet testset
 
 docker ps -a
 
--- cleanup.sh
+++ cleanup.sh
@@ -7,12 +7,6 @@ if [ $containers -ge 1 ]; then
   docker rm $(docker ps -qa)
 fi
 
-none_images=$(docker images -a|awk '/none/ {print $3}')
-
-for image in $none_images; do
-  docker rmi $image
-done
-
 #docker rmi $(docker images -qa)
 
 docker network rm my-mongo-cluster
