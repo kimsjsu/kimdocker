412729d0f10a3b0a1f6be0f4dc14393a2ce12ea1
Suneuy Kim <suneuy.kim@sjsu.edu>
2020-01-26 14:29:35 -0800

    latest updates...

--- Files-to-copy/setup-replication.sh
+++ Files-to-copy/setup-replication.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "\n\033[30;48;5;82m*** Running mongo commands on node1 to set up a replica set of 3 nodes (node1, node2, node3):\033[0m\n"
+
 run_mongo_cmd() {
   echo -e "\n\033[30;48;5;82mmongo> $1\033[0m\n"
   mongo --eval $1
@@ -12,4 +14,7 @@ run_mongo_cmd "rs.add('node3:27017')"
 run_mongo_cmd "rs.status()"
 run_mongo_cmd "rs.conf()"
 
+echo -e "\n\033[30;48;5;82m*** Finished setting up a replica set of 3 nodes (node1, node2, node3)\033[0m\n"
+cat /tmp/populate-testdata.js
+echo ""
 
