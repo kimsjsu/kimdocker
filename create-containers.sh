e9404edb146633cf985c7671bff0b49e7f70c1fe
Suneuy Kim <suneuy.kim@sjsu.edu>
2020-01-26 00:04:31 -0800

    latest updates...

--- create-containers.sh
+++ create-containers.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+create_container() {
+  docker run -td --name node$1 -h node$1 --net mycluster --ip 192.168.10.1$2 myimage:Dockerfile
+}
+
 if [ $# -eq 1 ]; then
   node_qty=$1
   if [[ $node_qty -gt 0 && $node_qty -lt 100 ]]; then
@@ -10,16 +14,16 @@ if [ $# -eq 1 ]; then
       else
         n1=$n
       fi
-      docker run -td --name node$n1 -h node$n1 --net mycluster --ip 192.168.10.1$n myimage:Dockerfile
+      create_container $n1 $n
     done
   else
     echo "Usage: $0 N where 0 < N < 100"
     exit 1
   fi
 else
-  docker run -td --name node1 -h node1 --net mycluster --ip 192.168.10.101 myimage:Dockerfile
-  docker run -td --name node2 -h node2 --net mycluster --ip 192.168.10.102 myimage:Dockerfile
-  docker run -td --name node3 -h node3 --net mycluster --ip 192.168.10.103 myimage:Dockerfile
+  for n in 1 2 3; do
+    docker ps -a |grep -q node$n ||create_container $n $n
+  done
 fi
 
 echo ""
